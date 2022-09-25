a310f66fb71ab75759f55d7aabed99fc6daf73ca
Andreas Isnes Nilsen <anilsen@bdocert.no>
2022-09-25 21:32:33 +0200

    renamed folders and files. Added auth config

--- frontend/app/js/app.js
+++ frontend/app/js/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom';
 import './app.css';
-import Navbar from './Components/Navbar/navbar';
-import Footer from './Components/Footer/footer';
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
 
 class App extends React.Component {
     render(){
