026a05d7c1c584aa93af839c4513b9dbf09e651f
Andreas Isnes Nilsen <anilsen@bdocert.no>
2022-09-25 12:55:36 +0200

    added container build

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,11 +12,14 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	game_version := 3
-	fmt.Printf("Super Mario %s\n", game_version)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello Backend",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
