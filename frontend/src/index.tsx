17d0b2e1c3b02cb89afd25e0f8635b35ee927aba
Andreas Isnes Nilsen <anilsen@bdocert.no>
2022-09-30 20:17:19 +0200

    added typescript and node

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { msalConfig } from './config/authconfig';
+import { PublicClientApplication } from '@azure/msal-browser';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const msalInstance = new PublicClientApplication(msalConfig)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App authentication={msalInstance}/>
   </React.StrictMode>
 );
 
